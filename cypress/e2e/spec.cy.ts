d7ae553b5cc779f9e1e3dc543e8f160d5ff86a21
SakoMe <melkonsa@gmail.com>
2022-06-06 09:00:44 -0700

    updated dependencies

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@ import { cy, describe, it } from 'local-cypress';
 
 describe('<App /> end to end', () => {
   it('user can visit root route and find starter text', () => {
-    cy.visit('/');
+    cy.visit('http://localhost:3000/');
     cy.findByRole('heading', { name: /start here/i }).should('exist');
     cy.findByRole('heading', { name: /start here/i }).should('be.visible');
     cy.findByRole('heading', { name: /start here/i }).should('contain', 'Start here');
