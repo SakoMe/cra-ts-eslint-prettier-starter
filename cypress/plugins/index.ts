012946674a55771349c6ec93d9445dbb3b321a6e
SakoMe <melkonsa@gmail.com>
2022-08-25 07:29:15 -0700

    fixed a few minor cypress / TS issues

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -16,7 +16,9 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-module.exports = (on, config) => {
+module.exports = () => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
+
+export {};
